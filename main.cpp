fb2930ba461f8cb60f6d2bfba6f0b327a48c9f28
yutotnh <57719497+yutotnh@users.noreply.github.com>
2020-02-10 18:48:08 +0000

    release candidate

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 
 static const int offset_id_number = 0x300; // 0 ~ 0x7E0
 
-CAN can(PA_11, PA_12);                // RD, TD
+CAN can(PA_11, PA_12); // RD, TD
 // BusIn dip_sw(PA_4, PA_3, PA_1, PA_0); //下位ビット ~ 上位ビット
 BusIn dip_sw(PB_3, PB_4, PB_5, PB_6); //下位ビット ~ 上位ビット
 /**/
@@ -17,7 +17,7 @@ static const int total_motor = 1;
 CANMDPeripheral motor[total_motor] = {
     //    CANMDPeripheral(can, PA_5, PA_6, PA_7, PB_3, PB_4, PA_0, PA_1),    // pwm3/2
     //    CANMDPeripheral(can, PA_8, PA_9, PA_10, PB_0, PB_1, PA_2, PA_4),   // pwm1/3
-    CANMDPeripheral(can, PB_0, PA_6, PA_7, PB_1, PA_5, NC, NC),
+    CANMDPeripheral(can, PB_0, PA_6, PA_7, PB_1, PA_5, PA_10, PA_9), // Version 1.2.0 ~ はPA_10とPA_9を交換
 };
 
 Serial pc(USBTX, USBRX);
@@ -43,7 +43,8 @@ int main()
     }
 
     can.attach(&get_data);
-    ticker.attach(&adjust, 0.1);
+    ticker.attach(&adjust, 0.001);
+    // ticker.attach(&adjust, 0.001);
 
     while (true)
     {
@@ -53,9 +54,10 @@ int main()
             if (motor[i].has_recieved_initial_value() == false)
             {
                 motor[i].connect();
-                printf("Please Connect!");
+                // printf("Please Connect!");
             }
         }
+        
     }
 }
 
@@ -66,7 +68,7 @@ void set_up_id()
 
     // TODO can filterを使う
     int rx_filter_id = dip_id * 16 + offset_id_number;
-    can.filter(rx_filter_id, 0x7F0, CANAny, 1); // handleは0にしたほうがいいかも
+    //can.filter(rx_filter_id, 0x7F0, CANAny, 1); // handleは0にしたほうがいいかも
 
     for (int i = 0; i < total_motor; i++)
     {
@@ -79,7 +81,7 @@ void set_up_id()
 
 void get_data()
 {
-//    printf("GET!");
+    //    printf("GET!");
     can.read(msg); // can.read(msg, 1);
     for (int i = 0; i < total_motor; i++)
     {

42a4163dbc60278fd5f890570fd5a2753168b762
yutotnh <57719497+yutotnh@users.noreply.github.com>
2021-09-30 16:10:46 +0000

    Update: Change Lite version.

--- main.cpp
+++ main.cpp
@@ -9,14 +9,11 @@
 static const int offset_id_number = 0x300; // 0 ~ 0x7E0
 
 CAN can(PA_11, PA_12); // RD, TD
-// BusIn dip_sw(PA_4, PA_3, PA_1, PA_0); //下位ビット ~ 上位ビット
 BusIn dip_sw(PB_3, PB_4, PB_5, PB_6); //下位ビット ~ 上位ビット
-/**/
+
 static const int total_motor = 1;
 
 CANMotorPeripheral motor[total_motor] = {
-    //    CANMotorPeripheral(can, PA_5, PA_6, PA_7, PB_3, PB_4, PA_0, PA_1),    // pwm3/2
-    //    CANMotorPeripheral(can, PA_8, PA_9, PA_10, PB_0, PB_1, PA_2, PA_4),   // pwm1/3
     CANMotorPeripheral(can, PB_0, PA_6, PA_7, PB_1, PA_5, PA_10, PA_9), // Version 1.2.0 ~ はPA_10とPA_9を交換
 };
 
@@ -34,31 +31,14 @@ void adjust();
 int main()
 {
     pc.baud(115200);
-    pc.printf("CAN MD Peripheral Solo 1.0.0\n");
+    pc.printf("CAN MD Peripheral Solo Lite 1.0.0\n");
 
     set_up_id();
-    for (int i = 0; i < total_motor; i++)
-    {
-        motor[i].connect();
-    }
-
     can.attach(&get_data);
     ticker.attach(&adjust, 0.001);
-    // ticker.attach(&adjust, 0.001);
 
     while (true)
-    {
-        wait_us(250000);
-        for (int i = 0; i < total_motor; i++)
-        {
-            if (motor[i].has_recieved_initial_value() == false)
-            {
-                motor[i].connect();
-                // printf("Please Connect!");
-            }
-        }
-        
-    }
+        ;
 }
 
 void set_up_id()
@@ -81,7 +61,7 @@ void set_up_id()
 
 void get_data()
 {
-    //    printf("GET!");
+    printf("GET!");
     can.read(msg); // can.read(msg, 1);
     for (int i = 0; i < total_motor; i++)
     {
